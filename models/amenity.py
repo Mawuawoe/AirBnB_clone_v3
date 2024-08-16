5a8609eb00f420a074e3e76d665b18966b420ffa
Mawuawoe <desmonddzakago@gmail.com>
2024-08-16 20:17:28 +0000

    task_3

--- models/amenity.py
+++ models/amenity.py
@@ -7,10 +7,12 @@ import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 print(models.storage_t)
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
-    if models.storage_t == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        print(models.storage_t)
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
