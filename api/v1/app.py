4dd67848e1c104f6c44ba42c14dab4e3e3ce2430
Mawuawoe <desmonddzakago@gmail.com>
2024-08-17 17:38:15 +0000

    task_5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Flask application setup for AirBnB clone project."""
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from os import getenv
 from api.v1.views import app_views
 
@@ -17,6 +17,13 @@ app.register_blueprint(app_views)
 def teardown(self):
     """Calls storage.close()"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handles 404 errors and returns a JSON response.
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
