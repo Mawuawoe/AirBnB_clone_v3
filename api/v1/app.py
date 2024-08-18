e4935793bc832c769f78520a1c44605ccfc8532e
Mawuawoe <desmonddzakago@gmail.com>
2024-08-18 19:23:02 +0000

    task_12

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # Set up CORS
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})  # Allow all origins for development
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+# Allow all origins for development
 
 # Register the blueprint app_views to the Flask instance
 app.register_blueprint(app_views)
